48f4801131e5a1a5bc0de8acbce8357a819419c7
emrepeker <158168264+emrepeker@users.noreply.github.com>
2025-07-21 15:53:21 +0300

    Path Conflict

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = FastAPI()
 async def root():
     return {"message":"Hello World"}    
 #Path parameters
+
+#Special occasion   item_id == user_id
+@app.get("/items/me")                # This has to come before from {item_id} /me has a priority
+async def read_user_me():
+    return {"Item id":"Current User"}
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id):
+async def read_item(item_id: int): #You can pass the type of parameter. May create parsing problems
     return {"Item id ":  item_id}   
+   
